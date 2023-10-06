e40727e87a24fc2e787252a5d2de3802c3bdafd7
voidplanet <kmrnharrison@gmail.com>
2023-10-05 20:07:19 -0400

    flashcards pt 2

--- flashcards-web102/src/Card.jsx
+++ flashcards-web102/src/Card.jsx
@@ -5,6 +5,8 @@ import { useState } from 'react';
 export default function Card(props){
   const [isFlipped, setIsFlipped] = useState(false);
   const [cardNum, setCardNum] = useState(0);
+  const [checkAnswer, setCheckAnswer] = useState('');
+  const [guess, setGuess] = useState('');
   const questions = ['How many planets are in our solar system?',
                     'What is the biggest planet in our solar system?',
                     'True or False: There is no sound in space.',
@@ -13,35 +15,66 @@ export default function Card(props){
                     'True or False: Black holes are so powerful that not even light can escape them.',
                     'True or False: Pluto is considered a planet.',
                     'Earth is located in which galaxy?',
-                    'Fill in the blank: Planets that exist outside our solar system are called _________.',
+                    'Fill in the blank: _________ is an umbrella term for any planet that is NOT a member of our solar system.',
                     'Which of these terms refers to the scientific study of space: \"Astronomy\" or \"Astrology\"'];
-  const answers = ['8 planets',
+  const answers = ['8',
                   'Jupiter', 
                   'True', 
-                  '\"Goldilocks\" or \"Habitable\"',
+                  'Goldilocks',
                   'Saturn',
                   'True',
-                  'False (As of 2006)',
-                  'Milky Way Galaxy',
-                  '\"Exoplanets\" or \"Extrasolar planets\"',
+                  'False',
+                  'Milky Way',
+                  'Exoplanet',
                   'Astronomy'];
+  const altAnswers = ['Eight',
+                  '', 
+                  '', 
+                  'Habitable',
+                  '',
+                  '',
+                  '',
+                  'Milky Way Galaxy',
+                  'Extrasolar',
+                  ''];
   
   const handleFlip = () => {setIsFlipped(!isFlipped);}
-  const handleCardNum = () => {
-        setCardNum(Math.floor(Math.random() * (questions.length)));
-        if (isFlipped == true) {
-            handleFlip();
-        }
+
+  const handleNext = () => {
+    if (cardNum + 1 < questions.length) setCardNum(cardNum + 1);
+    if (isFlipped) handleFlip();
+    setGuess('');
+    setCheckAnswer('');
+  }
+
+  const handlePrev = () => {
+    if (cardNum - 1 >= 0) setCardNum(cardNum - 1);
+    if (isFlipped) handleFlip();
+    setGuess('');
+    setCheckAnswer('');
+  }
+
+  const handleCheckAnswer = (event) => {
+    event.preventDefault();
+    if (guess == '') {
+      setCheckAnswer('wrong');
+      return;
     }
+    guess.toLowerCase() == answers[cardNum].toLowerCase() || guess.toLowerCase() == altAnswers[cardNum].toLowerCase() ? setCheckAnswer('correct') : setCheckAnswer('wrong');
+  }
 
   return (
     <>
     <p>Number of cards: {questions.length}</p>
     <div onClick={handleFlip} className="cardContainer">
-    <img className='cardImg' src={props.image}></img>
-    <p>{isFlipped ? answers[cardNum] : questions[cardNum]}</p>
+    <p>{isFlipped ? (altAnswers[cardNum] != '' && altAnswers[cardNum] != 'Eight' ? answers[cardNum] + ' / ' + altAnswers[cardNum] : answers[cardNum]) : questions[cardNum]}</p>
     </div>
-    <button onClick={handleCardNum}>&#62;</button>
+    <form>
+      Your Best Guess: <input type='text' value={guess} onChange={event => setGuess(event.target.value)} id={checkAnswer}></input>
+      <input type='submit' value='Submit' className='submitButton' onClick={handleCheckAnswer}></input>
+    </form>
+    <button onClick={handlePrev}>&#60;</button>
+    <button onClick={handleNext}>&#62;</button>
     </>
   )
 }
